bc552fa76e7327ae7e95834f72cc411df2c95d82
propardus <48132183+propardus@users.noreply.github.com>
2020-12-04 20:08:55 +0300

    Первая версия

---
+++ sort_and_remove_duplicates.py
@@ -0,0 +1,28 @@
+import os
+import glob
+from bs4 import BeautifulSoup
+import shutil
+
+num = 0
+for xml_file in glob.glob('*.xml'):
+    with open(xml_file, 'r', encoding='UTF-8') as f:
+        soup = BeautifulSoup(f.read(), "lxml-xml")
+    path_to_img = soup.select_one('path').text
+    if not os.path.exists(path_to_img):
+        print(f'Файл не найден! {path_to_img}')
+        continue
+    new_file_name = f'dolgonosik {num:02}'
+    new_img_name = new_file_name + '.jpg'
+    new_xml_name = new_file_name + '.xml'
+    new_path_to_img = os.path.abspath(os.path.join('images/', new_img_name))
+    new_path_to_xml = os.path.abspath(os.path.join('labels/', new_xml_name))
+    shutil.copy(path_to_img, new_path_to_img)
+    # soup.select_one('path').text = new_path_to_img
+    with open(new_path_to_xml, 'w', encoding='UTF-8') as f:
+        f.write(str(soup).replace(path_to_img, new_path_to_img))
+    print(f'Файл - OK! {path_to_img} -> {new_path_to_img}')
+    num += 1
+
+
+
+
